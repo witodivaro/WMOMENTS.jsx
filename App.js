e2227608b697ed57d0f9ef75e5bc8e821c6ea8f0
Vitaly Litvinovich <witodivaro@gmail.com>
2021-03-04 14:21:57 +0300

    feat: Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return <Text>App</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
